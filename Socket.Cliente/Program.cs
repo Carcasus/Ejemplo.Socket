f61e1f9a2d779a03fe18c17a49253dcebe40e3d9
azhu Gómez Arellano <Azhu@hotmail.es>
2022-01-26 09:40:10 +0100

    Calculadora socket V1.0  26 01 22

--- Socket.Cliente/Program.cs
+++ Socket.Cliente/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
-using System.Threading;
-using System.Linq;
 using LibreriaDeDatos;
 
 namespace Calculator.Cliente
@@ -18,23 +15,30 @@ namespace Calculator.Cliente
             double operando1 = 0;
             double operando2 = 0;
 
-            
-            var resultado= "";
-            
+            var resultado = "";
+
             Console.WriteLine("Escriba la operacion a realizar (add, sub, plus o div):");
             Console.WriteLine("------------------------\n");
             operador = Console.ReadLine();
-
-                Console.WriteLine("Escriba ahora los dos valores numericos a tratar");
+            try
+            {
+                Console.WriteLine("Escriba ahora los dos valores numericos a tratar, separados por enter");
+                Console.WriteLine("[VALOR 1]");
+                Console.WriteLine("[VALOR 2]");
                 operando1 = double.Parse(Console.ReadLine());
                 operando2 = double.Parse(Console.ReadLine());
+            }
+            catch (System.FormatException e)
+            {
+                Console.WriteLine("Error. el valor introducido no es numerico" + e.ToString());
+            }
 
             //Una vez declarado el operador y los numeros operandos, se pasara a una de las cuatro opciones en el metodo aparte
             resultado = posiblesOperadores(operador, operando1, operando2, resultado);
-            
-            Console.WriteLine("El resultado es " + resultado);
+
+            Console.WriteLine(resultado);
             Console.WriteLine("___________________________________________________");
-            Console.Write("Press any key to close the Calculator console app...");
+            Console.Write("Pulse cualquier tecla para cerrar la ejecucion de la consola...");
             Console.ReadKey();
         }
 
@@ -91,73 +95,72 @@ namespace Calculator.Cliente
             var resultado = "";
             try
             {
-                double num1 = 0;
-                double num2 = 0;
-                if (!double.TryParse(operacion.Operador1.ToString(), out num1) || !double.TryParse(operacion.Operador2.ToString(), out num2))
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                //Conectamos el cliente con el servidor, bien con el localhost o bien con una ip
+                IPAddress ipAddress = host.AddressList[0];
+                //IPAddress ipAddress = IPAddress.Parse("192.168.100.126");
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 2800);
+                // Creamos el socket por medio de TCP / IP
+                using Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+                try
                 {
-                    Console.WriteLine("La operacion dara error por que los operandos no son numericos");
+                    //Ponemos en metodo aparte toda la conexion (envio y recibimiento) del mensaje
+                    resultado = conexionServidor(sender, remoteEP, resultado, operacion); 
                 }
-                else
+                catch (ArgumentNullException ane)
                 {
-                    IPHostEntry host = Dns.GetHostEntry("localhost");
-                    //Conectamos el cliente con el servidor, bien con el localhost o bien con una ip
-                    IPAddress ipAddress = host.AddressList[0]; 
-                    //IPAddress ipAddress = IPAddress.Parse("192.168.100.126");
-
-                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, 2800);
-
-                    // Creamos el socket por medio de TCP / IP
-                    using Socket sender = new Socket(ipAddress.AddressFamily,
-                        SocketType.Stream, ProtocolType.Tcp);
-                    try
-                    {
-                        // Connect to Remote EndPoint
-                        sender.Connect(remoteEP);
-
-                        Console.WriteLine("Socket connected to {0}",
-                            sender.RemoteEndPoint.ToString());
-
-                        Console.WriteLine("Socket redad for {0}",
-                            sender.LocalEndPoint.ToString());
-
-                        //Serializamos el objeto de la operacion para poder enviarlo por el socket
-                        string jsonString = JsonSerializer.Serialize(operacion);
-                        var cacheEnvioOperacion = Encoding.UTF8.GetBytes(jsonString);
-
-                        // Send the data through the socket.
-                        int bytesSendOperador = sender.Send(cacheEnvioOperacion); //Pasar el objeto serializado
-
-                        // Receive the response from the remote device.
-                        byte[] bufferRec = new byte[1024];
-                        int bytesRec1 = sender.Receive(bufferRec);
-
-                        //Deserializamos el mensaje recibido
-                        resultado = Encoding.UTF8.GetString(bufferRec, 0, bytesRec1);
-                        // Release the socket.
-                        sender.Shutdown(SocketShutdown.Both);
-                        sender.Close();
-
-                        return resultado;
-
-                    }
-                    catch (ArgumentNullException ane)
-                    {
-                        Console.WriteLine("Error, No se estan pasando los suficientes datos : {0}", ane.ToString());
-                    }
-                    catch (SocketException se)
-                    {
-                        Console.WriteLine("Error en los sockets : {0}", se.ToString());
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Error, Fallo inesperado : {0}", e.ToString());
-                    }
+                    Console.WriteLine("Error, No se estan pasando los suficientes datos : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("Error en los sockets : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error, Fallo inesperado : {0}", e.ToString());
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
+            return resultado;
+        }
+
+        private static String conexionServidor(Socket sender, IPEndPoint remoteEP, string resultado, DatosOperacion operacion)
+        {
+            // Connect to Remote EndPoint
+            sender.Connect(remoteEP);
+            Console.WriteLine("Socket connected to {0}",
+                sender.RemoteEndPoint.ToString());
+            Console.WriteLine("Socket redad for {0}",
+                sender.LocalEndPoint.ToString());
+
+            //Serializamos el objeto de la operacion para poder enviarlo por el socket
+            string jsonString = JsonSerializer.Serialize(operacion);
+            var cacheEnvioOperacion = Encoding.UTF8.GetBytes(jsonString);
+
+            // Se envian los datos a traves del socket.
+            int bytesSendOperador = sender.Send(cacheEnvioOperacion); //Pasar el objeto serializado
+
+            resultado = retornoDesdeServidor(sender, resultado); //Recibimos el resultado desde el servidor en este metodo
+
+            return resultado;
+        }
+
+        private static String retornoDesdeServidor(Socket sender, string resultado)
+        {
+            // Recibimos respuesta desde el servidor.
+            byte[] bufferRec = new byte[1024];
+            int bytesRec1 = sender.Receive(bufferRec);
+
+            //Deserializamos el mensaje recibido
+            resultado = Encoding.UTF8.GetString(bufferRec, 0, bytesRec1);
+            // cerramos el socket.
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+
             return resultado;
         }
     }
--- Socket.Servidor/Program.cs
+++ Socket.Servidor/Program.cs
@@ -1,55 +1,34 @@
 ﻿using LibreriaDeDatos;
 using System;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Calculator.Servidor
 {
     internal class Program
     {
-        static void  Main(string[] args)
+        static void Main(string[] args)
         {
             double resultado = 0;
             IPHostEntry host = Dns.GetHostEntry("localhost");
             IPAddress ipAddress = host.AddressList[0];
-            //IPAddress ipAddress = IPAddress.Parse("ip escucha"); //En el caso de usar una ip que no sea localhost
+            //IPAddress ipAddress = IPAddress.Parse("ip cliente"); //En el caso de usar una ip que no sea localhost
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 2800);
             try
             {
-                // Create a Socket that will use Tcp protocol
+                // Creamos un socket que use el protocolo tcp
                 using Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                // A Socket must be associated with an endpoint using the Bind method
+                // Un socket debe estar asociado con un endpoint usando el metodo Bind
                 listener.Bind(localEndPoint);
-                // Specify how many requests a Socket can listen before it gives Server busy response.
-                // We will listen 10 requests at a time
+                // Especificamos que habra hasta 10 solicitudes antes de que el servidor entre en estado de ocupado
                 listener.Listen(10);
                 Console.WriteLine("Waiting for a connection ..." + listener.LocalEndPoint.ToString());
                 while (true)
                 {
-                    Socket handler = listener.Accept();
-                    Console.WriteLine("Socket connected to {0}",
-                        handler.RemoteEndPoint.ToString());
-                    var cacheMenaje = new byte[4096];
-                    int bytesMenaje = handler.Receive(cacheMenaje);
-                    var respuesta = "";
-                    if (bytesMenaje > 0) //Comprueba que no llega un mensaje vacio
-                    {
-                        var mensaje = Encoding.UTF8.GetString(cacheMenaje, 0, bytesMenaje);
-                        var obj = JsonSerializer.Deserialize<DatosOperacion>(mensaje); //Deserializamos el mensaje recibido del cliente
-
-                        respuesta = hacerLaOperacion(resultado, obj, respuesta, mensaje); //Tratamos los datos en un metodo aparte
-                    }
-                    var cacheRespuesta = Encoding.UTF8.GetBytes(respuesta);
-                    handler.Send(cacheRespuesta); //Se envia la respuesta al cliente
-                    Thread.Sleep(0);
-
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
+                    tratamientoMensaje(listener, resultado);
                 }
             }
             catch (Exception e)
@@ -58,6 +37,25 @@ namespace Calculator.Servidor
             }
             Console.WriteLine("\n Press any key to continue...");
             Console.ReadKey();
+        }
+
+        private static void tratamientoMensaje(Socket listener, double resultado)
+        {
+            Socket handler = listener.Accept();
+            Console.WriteLine("Socket connected to {0}",
+                handler.RemoteEndPoint.ToString());
+            var cacheMenaje = new byte[4096];
+            int bytesMenaje = handler.Receive(cacheMenaje);
+            var respuesta = "";
+            if (bytesMenaje > 0) //Comprueba que no llega un mensaje vacio
+            {
+                var mensaje = Encoding.UTF8.GetString(cacheMenaje, 0, bytesMenaje);
+                var obj = JsonSerializer.Deserialize<DatosOperacion>(mensaje); //Deserializamos el mensaje recibido del cliente
+
+                respuesta = hacerLaOperacion(resultado, obj, respuesta, mensaje); //Tratamos los datos en un metodo aparte
+            }
+
+            enviarRespuesta(respuesta, handler);
         }
 
         //Ejecutamos la operacion recibida de uno de los clientes, retornamos la respuesta, que sera enviada de vuelta al cliente.
@@ -87,12 +85,30 @@ namespace Calculator.Servidor
             }
             else if (obj.operacion == TipoOperacion.Division)
             {
-                resultado = obj.Operador1 / obj.Operador2;
-                respuesta = "La division entre " + obj.Operador1 + " y " + obj.Operador2 + " daria como resultado = " + resultado;
-
+                //Si el segundo operador es 0, impedimos que se haga calculo alguno
+                if (obj.Operador2 == 0.0)
+                {
+                    Console.WriteLine("No se dividir entre 0");
+                    respuesta = "No se dividir entre 0";
+                }
+                else
+                {
+                    resultado = obj.Operador1 / obj.Operador2;
+                    respuesta = "La division entre " + obj.Operador1 + " y " + obj.Operador2 + " daria como resultado = " + resultado;
+                }
                 Console.WriteLine("{0} -> {1}", mensaje, respuesta);
             }
             return respuesta;
+        }
+
+        private static void enviarRespuesta(string respuesta, Socket handler)
+        {
+            var cacheRespuesta = Encoding.UTF8.GetBytes(respuesta);
+            handler.Send(cacheRespuesta); //Se envia la respuesta al cliente
+            Thread.Sleep(0);
+
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
         }
     }
 }
